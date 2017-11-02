d667d81a81e23ed48ac809f94eeadc0f05041afc
BEM <seymayetisen@gmail.com>
2017-11-02 10:25:19 +0300

    isimgirme

---
+++ isimgirme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isimgirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("isim giriniz");
+                string isim = Console.ReadLine();
+           
+            do
+            {
+
+                if (isim == isim.Trim())
+                {
+                    Console.WriteLine(isim);
+                    break;
+                }
+                else if (isim.Trim().Length != isim.TrimStart().Length || isim.Trim().Length != isim.TrimEnd().Length)
+                {
+                    Console.WriteLine("yanlis karakter girdiniz tekrar giriniz...");
+                    Console.WriteLine("isim giriniz");
+                    isim = Console.ReadLine();
+
+                }
+            } while (isim != isim.Trim());
+            
+
+
+        }
+    }
+}
--- metindeBul/Program.cs
+++ metindeBul/Program.cs
@@ -13,26 +13,41 @@ namespace metindeBul
         {
             string metin = Console.ReadLine();
             var ayir = metin.Split(new[] { '.', ',', ';', ' ','?','!' });
+            string[] bulunanlar = new string[metin.Length];
             int say=0;
             
             for (int i = 0; i < ayir.Length; i++)
             {
-
-                for (int j = 0; j < ayir.Length; j++)
+                
+                for (int h = 0; h < bulunanlar.Length; h++)
                 {
-                    if (ayir[i]==ayir[j])
+                    if (ayir[i] == bulunanlar[h])
                     {
-                        say++;
-                        
+                        break;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < ayir.Length; j++)
+                        {
+
+                            if (ayir[i] == ayir[j])
+                            {
+                                say++;
+
+                            }
+
+                        }
                     }
                     
+
                 }
                 
-                    Console.WriteLine("{0} kelimesi {1} tane var", ayir[i], say);
+
+                Console.WriteLine("{0} kelimesi {1} tane var", bulunanlar[i], say);
                     say = 0;
-                
-                
-                
+
+                bulunanlar[i] = ayir[i];
+
             }
         }
     }
