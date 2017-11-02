4bb1fcf3aeb0d35e18b5eb11d25a74c63ae5dce4
BEM <seymayetisen@gmail.com>
2017-11-02 12:56:41 +0300

    kelime sayisi en fazla olanı bulma

--- enFazlaGecenKelimeyiBulma/Program.cs
+++ enFazlaGecenKelimeyiBulma/Program.cs
@@ -4,28 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace metindeBul
+namespace enFazlaGecenKelimeyiBulma
 {
     class Program
     {
-       
         static void Main(string[] args)
         {
             string metin = Console.ReadLine();
-            var ayir = metin.Split(new[] { '.', ',', ';', ' ','?','!' });
+            var ayir = metin.Split(new[] { '.', ',', ';', ' ', '?', '!' });
             string[] bulunanlar = new string[metin.Length];
-            int say=0;
-            
+            int say = 0;
+            int max = 0;
+            int al = 0;
+
             for (int i = 0; i < ayir.Length; i++)
             {
-                
+
                 for (int h = 0; h < bulunanlar.Length; h++)
                 {
                     if (ayir[i] == bulunanlar[h])
                     {
                         break;
                     }
-                    
+
                 }
                 for (int j = 0; j < ayir.Length; j++)
                 {
@@ -40,11 +41,18 @@ namespace metindeBul
 
 
                 Console.WriteLine("{0} kelimesi {1} tane var", ayir[i], say);
-                    say = 0;
+                al = say;
+                say = 0;
 
                 bulunanlar[i] = ayir[i];
 
             }
+            if (max<al)
+            {
+                max = al;
+            }
+            Console.WriteLine(max);
+        }
         }
     }
-}
+
---
+++ virgülCikar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virgülCikar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin = Console.ReadLine();
+            var ayir = metin.Split(new[] { '.', ',', ';', ' ', '?', '!' });
+            int max = 0;
+
+            for (int i = 0; i < ayir.Length; i++)
+            {
+                if (max<ayir[i].Length)
+                {
+                    max = ayir[i].Length;
+                }
+                
+            }
+            Console.WriteLine(max);
+
+        }
+    }
+}
